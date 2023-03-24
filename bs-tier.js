6a990ce7a1049b689683eb1077f42a321d2ab540
Andreas Kournoutas <andreaskournoutas@gmail.com>
2023-03-24 05:12:55 +0200

    Update bs-tier.js

--- bs-tier.js
+++ bs-tier.js
@@ -1,14 +1,14 @@
 export function calculateBootstrapTier(resultType) {
-    let documentWidth = document.body.clientHeight;
-    if (documentWidth < getComputedStyle(document.documentElement).getPropertyValue('--bs-breakpoint-sm')) {
+    let documentWidth = document.body.clientWidth;
+    if (documentWidth < parseInt(getComputedStyle(document.documentElement).getPropertyValue('--bs-breakpoint-sm').replace('px', ''))) {
         return resultType == 'literal' ? 'xs' : 0;
-    } else if (documentWidth < getComputedStyle(document.documentElement).getPropertyValue('--bs-breakpoint-md')) {
+    } else if (documentWidth < parseInt(getComputedStyle(document.documentElement).getPropertyValue('--bs-breakpoint-md').replace('px', ''))) {
         return resultType == 'literal' ? 'sm' : 1;
-    } else if (documentWidth < getComputedStyle(document.documentElement).getPropertyValue('--bs-breakpoint-lg')) {
+    } else if (documentWidth < parseInt(getComputedStyle(document.documentElement).getPropertyValue('--bs-breakpoint-lg').replace('px', ''))) {
         return resultType == 'literal' ? 'md' : 2;
-    } else if (documentWidth < getComputedStyle(document.documentElement).getPropertyValue('--bs-breakpoint-xl')) {
+    } else if (documentWidth < parseInt(getComputedStyle(document.documentElement).getPropertyValue('--bs-breakpoint-xl').replace('px', ''))) {
         return resultType == 'literal' ? 'lg' : 3;
-    } else if (documentWidth < getComputedStyle(document.documentElement).getPropertyValue('--bs-breakpoint-xxl')) {
+    } else if (documentWidth < parseInt(getComputedStyle(document.documentElement).getPropertyValue('--bs-breakpoint-xxl').replace('px', ''))) {
         return resultType == 'literal' ? 'xl' : 4;
     } else {
         return resultType == 'literal' ? 'xxl' : 5;
