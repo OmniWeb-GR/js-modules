aef29536bddd36a313a27445ef1d0d121f107ea4
Andreas Kournoutas <andreaskournoutas@gmail.com>
2023-03-27 07:00:23 +0300

    Update and rename screen-orientation.js to determineScreenOrientationjs

--- screen-orientation.js
+++
@@ -1,7 +0,0 @@
-export function screenOrientation() {
-    if (document.body.clientHeight > document.body.clientWidth) {
-        return 'portrait';
-    } else {
-        return 'landscape';
-    }
-}
